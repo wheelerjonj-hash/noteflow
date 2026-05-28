4d6e585db8e88219be12462eaf7337330849e2a0
Jon Wheeler <jon@beachliferentalsfl.com>
2026-05-28 23:17:13 +0000

    v5 - force new filenames to bust Android cache

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name]-[hash]-v3.js`,
-        chunkFileNames: `assets/[name]-[hash]-v3.js`,
-        assetFileNames: `assets/[name]-[hash]-v3.[ext]`
+        entryFileNames: `assets/[name]-v5-[hash].js`,
+        chunkFileNames: `assets/[name]-v5-[hash].js`,
+        assetFileNames: `assets/[name]-v5-[hash].[ext]`
       }
     }
   }
